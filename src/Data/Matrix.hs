4c4665aead83ea9277b62f854d3c08e828321675
oliegjio <oliegjio@gmail.com>
2018-05-10 13:44:43 +0300

    Added matrices and matrix operations

--- src/Data/Matrix.hs
+++ src/Data/Matrix.hs
@@ -1,16 +1,32 @@
-module Matrix (
-  matrix, toList, fromList, (|*|)
+module Data.Matrix (
+  Dim(..),
+  matrix,
+  fromList,
+  toList,
+  rows,
+  row,
+  columns,
+  column,
+  transpose,
+  transposed,
+  (|*|),
+  (|+|)
 ) where
 
 newtype Matrix a = Matrix [[a]] deriving (Show, Read, Eq)
+newtype Vector a = Vector [a] deriving (Show, Read, Eq)
+data Dim = Dim Int Int deriving (Show, Read, Eq)
 
 toList :: Matrix a -> [[a]]
 toList m = case m of Matrix a -> a
 
 fromList :: [[a]] -> Matrix a
-fromList = Matrix
+fromList [] = Matrix [[]]
+fromList xs = if all (== (length $ head xs)) (map length xs) then Matrix xs else Matrix [[]]
 
 matrix :: Int -> Int -> (Int -> Int -> a) -> Matrix a
+matrix 0 _ _ = Matrix [[]]
+matrix _ 0 _ = Matrix [[]]
 matrix r c f = Matrix [[f x y | y <- [1..c]] | x <- [1..r]]
 
 rows :: Matrix a -> Int
@@ -26,15 +42,31 @@ column :: Matrix a -> Int -> [a]
 column m n = map (!! n) (toList m)
 
 transpose :: Matrix a -> Matrix a
-transpose m = Matrix (map (column m) [0..n])
-  where n = columns m
+transpose (Matrix [[]]) = Matrix [[]]
+transpose m = Matrix (map (column m) [0..n]) where n = columns m - 1
+
+transposed :: Matrix a -> Matrix a -> Bool
+transposed (Matrix [[]]) _ = False
+transposed _ (Matrix [[]]) = False
+transposed a b = rows a == columns b || rows b == columns a
+
+dim :: Matrix a -> Dim
+dim m = Dim (rows m) (columns m)
 
 (*+*) :: Num a => [a] -> [a] -> a
 a *+* b = sum $ zipWith (*) a b
 
 (|*|) :: (Eq a, Num a) => Matrix a -> Matrix a -> Matrix a
-a |*| b = if rows a /= columns b || rows b /= columns a then Matrix [[]]
-  else Matrix [[row a n *+* column b t | t <- [0..s]] | n <- [0..l]]
+Matrix [[]] |*| _ = Matrix [[]]
+_ |*| Matrix [[]] = Matrix [[]]
+a |*| b = if transposed a b then Matrix [[row a n *+* column b t | t <- [0..s]] | n <- [0..l]] else Matrix [[]]
   where
     l = rows a - 1
-    s = rows b - 1
+    s = columns b - 1
+
+(|+|) :: Num a => Matrix a -> Matrix a -> Matrix a
+Matrix [[]] |+| _ = Matrix [[]]
+_ |+| Matrix [[]] = Matrix [[]]
+a |+| b = if dim a == dim b then Matrix $ zipWith (zipWith (+)) (toList a) (toList b) else Matrix [[]]
+
+
